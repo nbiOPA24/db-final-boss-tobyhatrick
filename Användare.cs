da6ab3e4de547616f94b8fccd457be57d7f753c7
Toby <toby.hatrick@gmail.com>
2025-03-01 23:34:32 +0100

    Små ändringar för DB

--- Användare.cs
+++ Användare.cs
@@ -1,14 +1,13 @@
-public class Användare  // Basklass för att hantera grundläggande som namn och roll för varje användartyp
+public class Användare
 {
+    protected readonly DatabaseRepository _dbRepository; // Skyddad så att underklasser kan använda den
     public string Namn { get; set; }
-    public string Roll { get; set; } // "Köpare, "Säljare, "Personal"
+    public string Roll { get; set; }
 
-
-    public Användare(string namn, string roll)
+    public Användare(string namn, string roll, DatabaseRepository dbRepository)
     {
         Namn = namn;
         Roll = roll;
+        _dbRepository = dbRepository ?? throw new ArgumentNullException(nameof(dbRepository));
     }
-
-
 }
